49274566fd31ee03cad324cf634a13abc72ffc6e
Samuel Tagliacozzo <samu.tagliacozzo@gmail.com>
2024-11-12 16:55:55 +0100

    Agg. Algoritmi e Strutture Dati
    - Aggiornamento Lezioni I, II, III
    - Aggiunta TodoList

--- 2° Anno/Algoritmi/Codici Algoritmi/algoritimi_fibonacci.py
+++ 2° Anno/Algoritmi/Codici Algoritmi/algoritimi_fibonacci.py
@@ -17,13 +17,15 @@ def fibonacci3(n: int) -> int:
     return Fib[n-1]
 
 def fibonacci4(n: int) -> int:
-    a = 1 # F_n-1
-    b = 1 # F_n-2
-    for _ in range(3, n+1):
-        c = a + b # F_n
-        a = b
-        b = c
-    return c
+	a = 1 # F_n-1
+	b = 1 # F_n-2
+	i = 3
+	while (i<=n):
+		c = a + b # F_n
+		a = b
+		b = c
+		i += 1
+	return c
 
 def fibonacci5(n: int) -> int:
     N = np.array([[1, 1], [1, 0]])
@@ -45,4 +47,4 @@ def potenzadiMatrice(A: np.array, k: int) -> np.array:
         return M
 
 # Ci mette del tempo considerevole a causa degli algoritmi meno efficienti
-print(fibonacci1(40) == fibonacci2(40) == fibonacci3(40) == fibonacci4(40) == fibonacci5(40) == fibonacci6(40))
+print(fibonacci1(40), fibonacci2(40), fibonacci3(40), fibonacci4(40), fibonacci5(40), fibonacci6(40))
